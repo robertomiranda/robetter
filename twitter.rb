b8113c844031a044a593c5034a276a6bdf532153
roberto <rjmaltamar@gmail.com>
2010-12-02 10:19:44 -0500

    "twitter 1.0"

--- twitter.rb
+++ twitter.rb
@@ -1,10 +1,16 @@
 require 'rubygems'
 require "sinatra"
-require 'twitter_ouauth'
+require 'twitter_oauth'
 
 before do
   @@config={'consumer_key'=>"143901814-yHmhBvtty49RuNB2edzapnHCyS4sKXpUAkATCjns",
     'consumer_secret'=>"eLPQmj2rQM65EN3DU4MSMJH5tr9OQI96IohtsUByxHk"}
   @client = TwitterOAuth::Client.new(:consumer_key => @@config['consumer_key'],
   :consumer_secret => @@config['consumer_secret'])  
+end
+
+get '/' do
+  @trends = @client.current_trends
+  @tweets = @client.public_timeline
+  erb:home
 end
