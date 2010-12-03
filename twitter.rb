ceadb1984c2204972aa749cd0503ec6c924cbec3
guilleiguaran <guilleiguaran@gmail.com>
2010-12-03 11:17:40 -0500

    Styling twitter.rb

--- twitter.rb
+++ twitter.rb
@@ -3,41 +3,35 @@ require "sinatra"
 require 'twitter_oauth'
 
 configure do
+  @@config = {'consumer_key'=>"AFcCvgpKoilQY506lYPGJQ", 'consumer_secret'=>"tRygHzQOlY9RofsVjEwhoDbTHJdULsx1bhBbbL82Jfs"
+              'callback_url' => "http://localhost:4567/connect/auth"}
   set :sessions, true
 end
 
 before do  
-  @@config={'consumer_key'=>"AFcCvgpKoilQY506lYPGJQ",
-    'consumer_secret'=>"tRygHzQOlY9RofsVjEwhoDbTHJdULsx1bhBbbL82Jfs"}
-  @@config['callback_url'] = "http://localhost:4567/connect/auth"
-  @@client = TwitterOAuth::Client.new(:consumer_key => @@config['consumer_key'], 
-                                      :consumer_secret => @@config['consumer_secret'],
-                                      :token => session[:access_token],
-                                      :secret => session[:secret_token]
-                                      )
+  @client = TwitterOAuth::Client.new(:consumer_key => @@config['consumer_key'], :consumer_secret => @@config['consumer_secret'], 
+                                     :token => session[:access_token], :secret => session[:secret_token])
 end
 
 get '/' do  
-  @tweets = @@client.public_timeline  
+  @tweets = @client.public_timeline  
   erb :home
 end
 
 get '/timeline'do
-  @tweets = @@client.friends_timeline
+  @tweets = @client.friends_timeline
   erb :tweets
 end
 
 post '/update' do
   p ".--------------------------------update-------------------------------"
-  @@client.update(params[:tweet])
+  @client.update(params[:tweet])
   redirect "/timeline"
 end
 
 get '/mentions' do
-  
-  @tweets = @@client.mentions
   p ".--------------------------------mentions-------------------------------"
-  p @tweets
+  @tweets = @client.mentions
   erb :tweets
 end
 
@@ -45,18 +39,16 @@ get '/connect/auth' do
 
   begin
   session[:oauth_verifier] = params[:oauth_verifier] if params[:oauth_verifier]
-  @access_token = @@client.authorize(
+  @access_token = @client.authorize(
      session[:request_token],
       session[:request_token_secret],
       :oauth_verifier => session[:oauth_verifier]
     )
-  rescue OAuth::Unauthorized
-  end
+  rescue OAuth::Unauthorized ; end
   
-  if @@client.authorized?    
+  if @client.authorized?    
       session[:access_token] = @access_token.token
       session[:secret_token] = @access_token.secret
-      session[:user] = true
       redirect '/timeline'
     else
       redirect '/'
@@ -64,14 +56,10 @@ get '/connect/auth' do
   
 end
 
-
-
 get '/connect' do  
-  request_token = @@client.request_token(:oauth_callback => @@config['callback_url'])
   p "---------------------connect-----------------------------------"
-  p session[:request_token] = request_token.token  
-  p session[:request_token_secret] = request_token.secret
-  p session[:request_token]
-  p session[:request_token_secret]
+  request_token = @client.request_token(:oauth_callback => @@config['callback_url'])
+  session[:request_token] = request_token.token  
+  session[:request_token_secret] = request_token.secret
   redirect request_token.authorize_url.gsub('authorize', 'authenticate') 
 end
